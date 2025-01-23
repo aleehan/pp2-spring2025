d4157c71c10fd17fe93c2c1659d7b4f65106f459
Murat Alikhan <alihanmurat44@gmail.com>
2025-01-23 18:41:36 +0500

    Booleans

---
+++ week2-lab2/booleans.py
@@ -0,0 +1,22 @@
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# Ex 1
+print(10 > 9) # True
+
+# Ex 2
+print(10 == 9) # False
+
+# Ex 3
+print(10 < 9) # False
+
+# Ex 4
+print(bool("abc")) # True
+
+# Ex 5
+print(bool(0)) # False
